bde325b7d45ef0028d8ab5f46e20d90db640f7e5
llsffaa <liulia.1758@gmail.com>
2021-11-01 20:40:08 +0200

    Added Project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <header className="header">
+        <img src="https://png.pngtree.com/png-vector/20190702/ourlarge/pngtree-cat-sitting-logo-template-png-image_1535021.jpg" />
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src='https://99px.ru/sstorage/53/2015/08/tmb_140353_6926.jpg'/>
+            <div>
+                pic+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
